fa6533298ced0a011f8923593124614006ee0161
Rishi <choukseyrishi89@gmail.com>
2025-05-30 01:12:04 +0530

    mobileNave

--- src/config/redux/reducer/authReducer/index.js
+++ src/config/redux/reducer/authReducer/index.js
@@ -60,7 +60,7 @@ const authSlice = createSlice({
           (state.message = action?.payload?.message || "login failed");
       })
       .addCase(registerUser.pending, (state) => {
-        (state.isLoading = true), (state.message = "Registring...");
+        (state.isLoading = true), (state.message = "Registering...");
       })
       .addCase(registerUser.fulfilled, (state, action) => {
         (state.isLoading = false),
@@ -68,13 +68,13 @@ const authSlice = createSlice({
           (state.isSuccess = true),
           (state.message =
             action?.payload?.message ||
-            "Registring succefull, Please login in");
+            "Registering succefull, Please login in");
       })
       .addCase(registerUser.rejected, (state, action) => {
         (state.isLoading = false),
           (state.isSuccess = false),
           (state.isError = true),
-          (state.message = action?.payload?.message || "Registring failed");
+          (state.message = action?.payload?.message || "Registering failed");
       })
       .addCase(getAboutUser.pending, (state) => {
         (state.isLoading = true), (state.message = "Fetching user...");
