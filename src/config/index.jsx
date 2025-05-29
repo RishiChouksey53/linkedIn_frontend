8500525302c5e5090e577a3476afb4f46c706ea1
Rishi <choukseyrishi89@gmail.com>
2025-05-29 13:52:49 +0530

    baseUrl

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
-export const BASE_URL = "http://localhost:9090";
-// export const BASE_URL = "https://proconnect-h9gk.onrender.com";
+// export const BASE_URL = "http://localhost:9090";
+export const BASE_URL = "https://proconnect-h9gk.onrender.com";
 
 export const clientServer = axios.create({
   baseURL: BASE_URL,
